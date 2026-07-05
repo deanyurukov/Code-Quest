0dd9a5b87ddd5a5b34c05cb2f8838791999ac2d7
Dean Yurukov <dean.yurukov2008@gmail.com>
2026-07-06 00:19:42 +0300

    change cron execution time

--- server/src/cron/daily-question-cron.js
+++ server/src/cron/daily-question-cron.js
@@ -3,13 +3,10 @@ import { getAIResponse } from "../services/ai-service.js";
 import Question from "../models/question.js";
 
 cron.schedule(
-    "01 0 * * *",
+    "0 0 * * *",
     async () => {
         console.log("Generating today's question...");
         await getAIResponse();
         console.log("Today's question was generated and saved to the database.");
-    },
-    {
-        timezone: "Europe/Sofia",
     }
 );
