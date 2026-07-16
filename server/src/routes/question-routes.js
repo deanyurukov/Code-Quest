caa4917456db203e07d31831bc91a78c49549f3a
Dean Yurukov <dean.yurukov2008@gmail.com>
2026-07-16 10:40:31 +0200

    fix object error

--- server/src/routes/question-routes.js
+++ server/src/routes/question-routes.js
@@ -5,7 +5,13 @@ import { LAUNCH_DATE } from "../server.js";
 const router = express.Router();
 
 router.get("/question/get", async (req, res) => {
-    const response = (await getQuestion()).toObject();
+    const question = await getQuestion();
+
+    if (!question) {
+        return res.status(404).json({ error: "No question found for today" });
+    }
+
+    const response = question.toObject();
     const dayNumber = Math.floor((new Date(response.date) - LAUNCH_DATE) / (1000 * 60 * 60 * 24)) + 1;
     response.dayNumber = dayNumber;
     res.json(response);
