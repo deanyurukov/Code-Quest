4d93c3b1a1e4ef48615ae34519391ce89b919b07
Dean Yurukov <dean.yurukov2008@gmail.com>
2026-07-25 14:25:09 +0300

    server now returns previous and next day question info

--- server/src/routes/question-routes.js
+++ server/src/routes/question-routes.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { getQuestion } from "../services/question-service.js";
 import { LAUNCH_DATE } from "../server.js";
+import Question from "../models/question.js";
+import { getSofiaDateString } from "../services/date-service.js";
 
 const router = express.Router();
 
@@ -13,7 +15,19 @@ router.get("/question/get", async (req, res) => {
 
     const response = question.toObject();
     const dayNumber = Math.floor((new Date(response.date) - LAUNCH_DATE) / (1000 * 60 * 60 * 24)) + 1;
+    
+    const previous = new Date(response.date);
+    previous.setDate(previous.getDate() - 1);
+    const previousExists = (await Question.find({ date: getSofiaDateString(previous) })).length !== 0;
+    
+    const next = new Date(response.date);
+    next.setDate(next.getDate() + 1);
+    const nextExists = (await Question.find({ date: getSofiaDateString(next) })).length !== 0;
+    
     response.dayNumber = dayNumber;
+    response.previousExists = previousExists;
+    response.nextExists = nextExists;
+
     res.json(response);
 });
 
