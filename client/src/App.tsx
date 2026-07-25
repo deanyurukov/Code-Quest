bc92b462fd78a860a76e1e4ea397a76fdb3fd761
Dean Yurukov <dean.yurukov2008@gmail.com>
2026-07-25 14:56:54 +0300

    add day change logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,7 +12,9 @@ function App() {
         topic: string,
         difficulty: string,
         correctAnswerIndex: number,
-        dayNumber: number
+        dayNumber: number,
+        previousExists: boolean,
+        nextExists: boolean
     }
 
     const [question, setQuestion] = useState<Question | null>(null);
@@ -23,6 +25,18 @@ function App() {
     async function getQuestion(): Promise<void> {
         const data = await get<Question>(endpoints.today);
         setQuestion(data);
+    }
+
+    async function getDiffQuestion(offset: number) {
+        const wantedDay = new Date(question!.date);
+        wantedDay.setDate(wantedDay.getDate() + offset);
+
+        const formattedDate = wantedDay.toISOString().split("T")[0];
+
+        const newQuestion = await get<Question>(endpoints.specific(formattedDate));
+        setQuestion(newQuestion);
+        setIsSubmitted(false);
+        setSelectedAnswer(null);
     }
 
     function handleSubmit(e: any): void {
@@ -43,15 +57,15 @@ function App() {
                 </section>
 
                 <section className="day">
-                    <button>
+                    <button disabled={question?.previousExists === false} onClick={() => getDiffQuestion(-1)} >
                         <span>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" data-fg-d3bl34="0.8:0.125:node_modules/lucide-react:339:31:12390:35:e:ChevronLeft::::::O1c" data-fgid-d3bl34=":r11:"><path d="m15 18-6-6 6-6"></path></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" data-fg-d3bl34="0.8:0.125:node_modules/lucide-react:339:31:12390:35:e:ChevronLeft::::::O1c" data-fgid-d3bl34=":r11:"><path d="m15 18-6-6 6-6"></path></svg>
                         </span>
                     </button>
                     <h2>Day {question?.dayNumber}</h2>
-                    <button disabled>
+                    <button disabled={question?.nextExists === false} onClick={() => getDiffQuestion(1)} >
                         <span>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" data-fg-d3bl35="0.8:0.125:node_modules/lucide-react:339:69:12428:36:e:ChevronRight::::::ByYJ" data-fgid-d3bl35=":r18:"><path d="m9 18 6-6-6-6"></path></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" data-fg-d3bl35="0.8:0.125:node_modules/lucide-react:339:69:12428:36:e:ChevronRight::::::ByYJ" data-fgid-d3bl35=":r18:"><path d="m9 18 6-6-6-6"></path></svg>
                         </span>
                     </button>
                 </section>
@@ -82,7 +96,7 @@ function App() {
                     }
                 </section>
 
-                { !isSubmitted && <button disabled={selectedAnswer === null} onClick={handleSubmit}>Submit Answer</button> }
+                {!isSubmitted && <button disabled={selectedAnswer === null} onClick={handleSubmit}>Submit Answer</button>}
             </main>
         </>
     );
--- client/src/api/endpoints.ts
+++ client/src/api/endpoints.ts
@@ -1,5 +1,6 @@
 const baseUrl = "http://localhost:5001";
 
 export const endpoints = {
-    today: `${baseUrl}/question/get`
+    today: `${baseUrl}/question/get`,
+    specific: (day: string) => `${baseUrl}/question/${day}`
 };
