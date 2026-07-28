f7b6cbf5291410bb716c6aaeb8e8d10cda09ecff
Dean Yurukov <dean.yurukov2008@gmail.com>
2026-07-28 15:09:12 +0300

    site responsiveness improvement 2 & general improvements

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import "../public/styles/styles.css";
 import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')!).render(
