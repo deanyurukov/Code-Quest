763872d8a539e8de44f124b71a8eb8a21d8c05b4
Dean Yurukov <dean.yurukov2008@gmail.com>
2026-08-07 18:17:52 +0300

    update book svg to align to text

--- client/src/components/QuestionExplanation.tsx
+++ client/src/components/QuestionExplanation.tsx
@@ -10,7 +10,7 @@ const QuestionExplanation = ({ explanation, isCorrect }: { explanation: string, 
                 <h5>{isCorrect ? "Quest complete! That's correct!" : "Fallen warrior - here's the lesson:"}</h5>
             </article>
             <article>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M12 7v14"></path><path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z"></path></svg>
+                <svg className="book" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M12 7v14"></path><path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z"></path></svg>
                 <p>{explanation}</p>
             </article>
         </section>
